4646788d3d6166acf212e3c526ce84ae8545c5d1
Stifler33 <bagi33.ru@gmail.com>
2025-10-21 13:57:53 +0300

    удалил лишнее

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,12 @@
 #include <template.h>
 #include <stifler_mqtt.h>
 
-void test(){
-    Serial.print("test handler");    
-}
-
 void setup(){
     set_pin_reset(12, HIGH);
     init_stif();
     arduino_ota_initial();
     add_pub_topic("connect", "esp32/connect");
-    add_sub_topic("test_sub", "esp32/test_sub", test);
+    add_sub_topic("test_sub", "esp32/test_sub");
     init_brocker();
 }
 
